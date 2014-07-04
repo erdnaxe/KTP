ce5b039a389c8af8462579b3ef8e39f5b77e34c4
erdnaxe <alexandre.iooss@sfr.fr>
2014-07-04 08:36:41 +0200

    Change the command to /ktp and format all files.

--- README.md
+++ README.md
@@ -8,6 +8,40 @@ Les principaux ajouts sont :
  * Comptatible 1.7.9
  * Une nouvelle barre de temps
 
+Utilisation
+==========
+
+Pour créer un nouveau point de spawn :
+```
+/ktp addspawn
+```
+
+Pour démarrer le ktp :
+
+```
+/ktp start
+```
+
+Pour sauter d'un épisode :
+```
+/ktp shift
+```
+
+Pour créer les teams :
+```
+/ktp team
+```
+
+Pour modifier la taille de la map :
+```
+/ktp size
+```
+
+Pour génerer les murs :
+```
+/ktp generatewalls
+```
+
 Licence
 =======
 
---
+++ src/main/java/me/azenet/KTPPlugin/KTPTeam.java
@@ -0,0 +1,60 @@
+package me.azenet.KTPPlugin;
+
+import java.util.ArrayList;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+
+public class KTPTeam {
+
+    private String name;
+    private String displayName;
+    private ChatColor color;
+    private KTPPlugin plugin;
+    private ArrayList<Player> players = new ArrayList<Player>();
+
+    public KTPTeam(String name, String displayName, ChatColor color, KTPPlugin plugin) {
+        this.name = name;
+        this.displayName = displayName;
+        this.color = color;
+        this.plugin = plugin;
+
+        Scoreboard sb = this.plugin.getScoreboard();
+        sb.registerNewTeam(this.name);
+
+        Team t = sb.getTeam(this.name);
+        t.setDisplayName(this.displayName);
+        t.setCanSeeFriendlyInvisibles(true);
+        t.setPrefix(this.color + "");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public void addPlayer(Player playerExact) {
+        players.add(playerExact);
+        plugin.getScoreboard().getTeam(this.name).addPlayer(playerExact);
+    }
+
+    public void teleportTo(Location lo) {
+        for (Player p : players) {
+            p.teleport(lo);
+        }
+    }
+
+    public ChatColor getChatColor() {
+        return color;
+    }
+}
