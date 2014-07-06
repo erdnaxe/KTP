a7ace4d559e6462825c1e82a0d5a45f22d91b3c8
erdnaxe <alexandre.iooss@sfr.fr>
2014-07-06 11:50:48 +0200

    New release ! Stable.

--- src/main/java/me/azenet/KTPPlugin/KTPTeam.java
+++ src/main/java/me/azenet/KTPPlugin/KTPTeam.java
@@ -9,27 +9,47 @@ import org.bukkit.scoreboard.Team;
 
 public class KTPTeam {
 
+    private final Scoreboard board;
     private String name = "Team";
     private String displayName = "Team";
     private ChatColor color = ChatColor.WHITE;
     private ArrayList<Player> players = new ArrayList<Player>();
-    private Scoreboard board;
 
+    /**
+     * Constructeur de la classe KTPTeam
+     * @param sb Main scoreboard
+     */
     public KTPTeam(Scoreboard sb) {
         board = sb; // On récupère le scoreboard
 
+        // On supprime les anciennes teams
+        try {
+            board.getTeam(this.name).unregister();
+        } catch (IllegalArgumentException e) {
+        }
+
+        // On crée une nouvelle team
         board.registerNewTeam(this.name);
 
+        // On paramètre la team
         Team t = board.getTeam(this.name);
         t.setDisplayName(this.displayName);
         t.setCanSeeFriendlyInvisibles(true);
         t.setPrefix(this.color + "");
     }
 
+    /**
+     * Récupérer le nom de la team.
+     * @return Nom de la team
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Modifie le nom de la team.
+     * @param n Nom de la team.
+     */
     public void setName(String n) {
         name = n;
     }
